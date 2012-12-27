17a5a664bc2fb5e6f79617a2a2c165da4bd8066d
mfp <michelf.portzert@gmail.com>
2012-12-27 03:07:00 +0100

    Added Ressource footer

--- src/com/quizz/places/activities/MenuActivity.java
+++ src/com/quizz/places/activities/MenuActivity.java
@@ -1,5 +1,5 @@
 package com.quizz.places.activities;
-	
+
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MotionEvent;
@@ -15,8 +15,11 @@ import com.actionbarsherlock.internal.nineoldandroids.animation.Animator;
 import com.actionbarsherlock.internal.nineoldandroids.animation.Animator.AnimatorListener;
 import com.actionbarsherlock.internal.nineoldandroids.animation.AnimatorSet;
 import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
+import com.actionbarsherlock.internal.nineoldandroids.animation.ValueAnimator;
+import com.actionbarsherlock.internal.nineoldandroids.animation.ValueAnimator.AnimatorUpdateListener;
 import com.quizz.core.activities.BaseMenuActivity;
 import com.quizz.places.R;
+import com.quizz.places.widgets.MenuBackground;
 
 public class MenuActivity extends BaseMenuActivity {
 
@@ -27,7 +30,9 @@ public class MenuActivity extends BaseMenuActivity {
 	private Button mButtonQuit;
 	private ImageView mTitleSign;
 	private ImageView mClouds;
-
+	private ImageView mFooter;
+	private MenuBackground mHaloBackground;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -41,44 +46,74 @@ public class MenuActivity extends BaseMenuActivity {
 		mButtonQuit = (Button) findViewById(R.id.buttonQuit);
 		mTitleSign = (ImageView) findViewById(R.id.titleSign);
 		mClouds = (ImageView) findViewById(R.id.clouds);
+		mFooter = (ImageView) findViewById(R.id.footer);
+		//mHaloBackground = (MenuBackground) findViewById(R.id.haloBackground);
 		
-		// FIXME: May not be displayed correctly on bigger screen when looping (bad transition)
+		// FIXME: May not be displayed correctly on bigger screen when looping
+		// (bad transition)
 		// TODO: Make an image with beginning left similar to right end
-		// TODO: Scroll the horizontalScrollView instead of translating the imageView
+		// TODO: Scroll the horizontalScrollView instead of translating the
+		// imageView
 		HorizontalScrollView cloudsScrollView = (HorizontalScrollView) findViewById(R.id.cloudsContainer);
 		cloudsScrollView.setOnTouchListener(new OnTouchListener() {
-			
+
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				return true;
 			}
 		});
-		
+
 		startAnimations();
 	}
-	
+
 	private void startAnimations() {
-		
-		ObjectAnimator signPopup = ObjectAnimator.ofFloat(mTitleSign, "translationY", -200, 0);
+
+		ObjectAnimator signPopup = ObjectAnimator.ofFloat(mTitleSign,
+				"translationY", -200, 0);
 		signPopup.setDuration(300);
 		signPopup.addListener(mSignAnimatorListener);
-		
-		ObjectAnimator signBounceUp = ObjectAnimator.ofFloat(mTitleSign, "translationY", 0, -5);
+
+		ObjectAnimator signBounceUp = ObjectAnimator.ofFloat(mTitleSign,
+				"translationY", 0, -5);
 		signBounceUp.setDuration(100);
-		
-		ObjectAnimator signBounceDown = ObjectAnimator.ofFloat(mTitleSign, "translationY", -5, 0);
+
+		ObjectAnimator signBounceDown = ObjectAnimator.ofFloat(mTitleSign,
+				"translationY", -5, 0);
 		signBounceDown.setDuration(100);
-		
+
 		AnimatorSet signAnimatorSet = new AnimatorSet();
-		signAnimatorSet.playSequentially(signPopup, signBounceUp, signBounceDown);
+		signAnimatorSet.playSequentially(signPopup, signBounceUp,
+				signBounceDown);
 		signAnimatorSet.setStartDelay(500);
 		signAnimatorSet.start();
-		
-		ObjectAnimator cloudMoving = ObjectAnimator.ofFloat(mClouds, "translationX", 500, -1700);
+
+		ObjectAnimator cloudMoving = ObjectAnimator.ofFloat(mClouds,
+				"translationX", 500, -1700);
 		cloudMoving.setDuration(30000);
 		cloudMoving.setInterpolator(new LinearInterpolator());
 		cloudMoving.setRepeatCount(ObjectAnimator.INFINITE);
 		cloudMoving.start();
+
+		ObjectAnimator footerPopup = ObjectAnimator.ofFloat(mFooter,
+				"translationY", 500, 0);
+		footerPopup.setDuration(500);
+		footerPopup.setStartDelay(500);
+		footerPopup.addListener(mFooterAnimatorListener);
+		footerPopup.start();
+/*
+		ValueAnimator animator = ValueAnimator.ofFloat(0, 360);
+		animator.addUpdateListener(new AnimatorUpdateListener() {
+			@Override
+			public void onAnimationUpdate(ValueAnimator arg0) {
+
+				mHaloBackground.rotationDegrees = (Float) arg0.getAnimatedValue();
+				mHaloBackground.invalidate();
+			}
+		});
+		animator.setInterpolator(new LinearInterpolator());
+		animator.setRepeatCount(ValueAnimator.INFINITE);
+		animator.setDuration(8000);
+		animator.start();*/
 	}
 
 	@Override
@@ -86,25 +121,48 @@ public class MenuActivity extends BaseMenuActivity {
 		getMenuInflater().inflate(R.menu.activity_menu, menu);
 		return true;
 	}
-	
+
 	// ===========================================================
-    // Listeners
-    // ===========================================================
-	
+	// Listeners
+	// ===========================================================
+
 	AnimatorListener mSignAnimatorListener = new AnimatorListener() {
-		
+
 		@Override
 		public void onAnimationStart(Animator animation) {
 			mTitleSign.setVisibility(View.VISIBLE);
 		}
 
 		@Override
-		public void onAnimationEnd(Animator animation) {}
+		public void onAnimationEnd(Animator animation) {
+		}
 
 		@Override
-		public void onAnimationCancel(Animator animation) {}
+		public void onAnimationCancel(Animator animation) {
+		}
 
 		@Override
-		public void onAnimationRepeat(Animator animation) {}
+		public void onAnimationRepeat(Animator animation) {
+		}
+	};
+
+	AnimatorListener mFooterAnimatorListener = new AnimatorListener() {
+
+		@Override
+		public void onAnimationStart(Animator animation) {
+			mFooter.setVisibility(View.VISIBLE);
+		}
+
+		@Override
+		public void onAnimationEnd(Animator animation) {
+		}
+
+		@Override
+		public void onAnimationCancel(Animator animation) {
+		}
+
+		@Override
+		public void onAnimationRepeat(Animator animation) {
+		}
 	};
 }
