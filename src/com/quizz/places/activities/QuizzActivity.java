d64ad18c4a0660595c827ee6999dfeaeb565d67d
mfportzert <michelf.portzert@gmail.com>
2012-12-30 01:45:00 +0100

    Implemented quizz background animation

--- src/com/quizz/places/activities/QuizzActivity.java
+++ src/com/quizz/places/activities/QuizzActivity.java
@@ -1,8 +1,14 @@
 package com.quizz.places.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
+import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
 import com.quizz.core.activities.BaseQuizzActivity;
+import com.quizz.core.utils.NavigationUtils;
+import com.quizz.places.R;
 import com.quizz.places.fragments.MenuFragment;
 
 public class QuizzActivity extends BaseQuizzActivity {
@@ -11,6 +17,18 @@ public class QuizzActivity extends BaseQuizzActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		navigateTo(MenuFragment.class);
+		NavigationUtils.navigateTo(MenuFragment.class, getSupportFragmentManager(), this);
+		
+		View quizzLayout = getQuizzLayout();
+		quizzLayout.setBackgroundResource(R.drawable.sky_clean);
+		
+		ImageView backgroundImage = getBackgroundAnimatedImage();
+		backgroundImage.setBackgroundResource(R.drawable.clouds);
+		
+		ObjectAnimator bgAnimation = ObjectAnimator.ofFloat(backgroundImage, "translationX", 500, -1700);
+		bgAnimation.setDuration(30000);
+		bgAnimation.setInterpolator(new LinearInterpolator());
+		bgAnimation.setRepeatCount(ObjectAnimator.INFINITE);
+		bgAnimation.start();
 	}
 }
