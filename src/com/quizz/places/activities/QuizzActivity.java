6ddad3f9a6518195faf33e107386f6fb4f87cbcb
mfportzert <michelf.portzert@gmail.com>
2012-12-30 23:57:30 +0100

    Fix: Orientation changes not keeping fragments backstack

--- src/com/quizz/places/activities/QuizzActivity.java
+++ src/com/quizz/places/activities/QuizzActivity.java
@@ -1,13 +1,12 @@
 package com.quizz.places.activities;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.LinearInterpolator;
-import android.widget.ImageView;
 
 import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
 import com.quizz.core.activities.BaseQuizzActivity;
 import com.quizz.core.utils.NavigationUtils;
+import com.quizz.core.widgets.QuizzActionBar;
 import com.quizz.places.R;
 import com.quizz.places.fragments.MenuFragment;
 
@@ -17,18 +16,30 @@ public class QuizzActivity extends BaseQuizzActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		NavigationUtils.directNavigationTo(MenuFragment.class, getSupportFragmentManager(), this, false);
+		if (savedInstanceState == null) {
+			NavigationUtils.directNavigationTo(MenuFragment.class, getSupportFragmentManager(), 
+					this, false);
+		}
+		
 		/*
 		View confirmQuitDialog = getLayoutInflater().inflate(R.layout.dialog_confirm_quit, null);
 		setConfirmQuitDialogView(confirmQuitDialog);
 		*/
-		View quizzLayout = getQuizzLayout();
-		quizzLayout.setBackgroundResource(R.drawable.sky_clean);
 		
-		ImageView backgroundImage = getBackgroundAnimatedImage();
-		backgroundImage.setBackgroundResource(R.drawable.clouds);
+		getQuizzActionBar().hide(QuizzActionBar.MOVE_DIRECT);
+		getQuizzActionBar().getBackButton().setImageResource(R.drawable.back_but_1);
+		getQuizzActionBar().getMiddleText().setText("22 / 30");
+		getQuizzActionBar().getRightText().setText("345 pts");
 		
-		ObjectAnimator bgAnimation = ObjectAnimator.ofFloat(backgroundImage, "translationX", 500, -1700);
+		initBackground();
+	}
+	
+	private void initBackground() {
+		getQuizzLayout().setBackgroundResource(R.drawable.sky_clean);
+		getBackgroundAnimatedImage().setBackgroundResource(R.drawable.clouds);
+		
+		ObjectAnimator bgAnimation = ObjectAnimator.ofFloat(getBackgroundAnimatedImage(), 
+				"translationX", 500, -1700);
 		bgAnimation.setDuration(30000);
 		bgAnimation.setInterpolator(new LinearInterpolator());
 		bgAnimation.setRepeatCount(ObjectAnimator.INFINITE);
