b3834fc9e50ea06b890da10ace99ac5b913817ba
mfportzert <michelf.portzert@gmail.com>
2013-01-14 02:36:00 +0100

    Added difficulty design on grid

--- src/com/quizz/places/adapters/LevelsItemAdapter.java
+++ src/com/quizz/places/adapters/LevelsItemAdapter.java
@@ -1,18 +1,20 @@
 package com.quizz.places.adapters;
 
 import java.io.IOException;
+import java.lang.ref.WeakReference;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.quizz.core.models.Level;
 import com.quizz.places.R;
@@ -21,7 +23,7 @@ public class LevelsItemAdapter extends ArrayAdapter<Level> {
 
 	private int mLineLayout;
 	private LayoutInflater mInflater;
-	private SparseArray<Drawable> mPictures = new SparseArray<Drawable>();
+	private SparseArray<WeakReference<Drawable>> mPictures = new SparseArray<WeakReference<Drawable>>();
 	
 	public LevelsItemAdapter(Context context, int lineLayout) {
         super(context, lineLayout);
@@ -33,7 +35,7 @@ public class LevelsItemAdapter extends ArrayAdapter<Level> {
 	static class ViewHolder {
 		int position;
         ImageView picture;
-        ImageView difficulty;
+        LinearLayout difficulty;
         ImageView statusIcon;
     }
 	
@@ -41,27 +43,32 @@ public class LevelsItemAdapter extends ArrayAdapter<Level> {
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
         if (convertView == null) {
-            
         	convertView = this.mInflater.inflate(this.mLineLayout, null);
         	
             holder = new ViewHolder();
             holder.picture = (ImageView) convertView.findViewById(R.id.levelPicture);
-            holder.difficulty = (ImageView) convertView.findViewById(R.id.levelDifficulty);
+            holder.difficulty = (LinearLayout) convertView.findViewById(R.id.levelDifficulty);
             holder.statusIcon = (ImageView) convertView.findViewById(R.id.levelStatusIcon);
+            //holder.picture.setAlpha(0);
             holder.picture.setVisibility(View.GONE);
+            holder.difficulty.setVisibility(View.GONE);
             
             convertView.setTag(holder);
-            
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
         
         Level level = getItem(position);
         holder.position = position;
-        if (mPictures.get(position) != null) {
-        	holder.picture.setImageDrawable(mPictures.get(position));
+        
+        Drawable picture = null;
+        WeakReference<Drawable> pictureRef = mPictures.get(position);
+        if (pictureRef != null) picture = pictureRef.get();
+        
+        if (picture != null) {
+        	holder.picture.setImageDrawable(picture);
+            holder.difficulty.setVisibility(View.VISIBLE);
         } else {
-        	// FIXME: position system is probably not working
         	new ThumbnailTask(position, holder).execute();
         }
         
@@ -90,12 +97,13 @@ public class LevelsItemAdapter extends ArrayAdapter<Level> {
 	    @Override
 	    protected void onPostExecute(Drawable drawable) {
 	        if (mHolder.position == mPosition) {
-	        	mPictures.append(mPosition, drawable);
+	        	mPictures.append(mPosition, new WeakReference<Drawable>(drawable));
 	            mHolder.picture.setImageDrawable(drawable);
-	            mHolder.picture.setVisibility(View.VISIBLE);
-	            
-/*	            Animation animation = AnimationUtils.loadAnimation(getContext(), android.R.anim.fade_in);
+	            /*
+	            Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
 	    		mHolder.picture.startAnimation(animation);*/
+	    		mHolder.picture.setVisibility(View.VISIBLE);
+	    		mHolder.difficulty.setVisibility(View.VISIBLE);
 	        }
 	    }
 	}
