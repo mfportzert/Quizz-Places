66028e024eb882d38a635582c9876748c922bab4
mfportzert <michelf.portzert@gmail.com>
2013-01-15 01:39:19 +0100

    Levels: better transitions

--- src/com/quizz/places/fragments/GridLevelsFragment.java
+++ src/com/quizz/places/fragments/GridLevelsFragment.java
@@ -3,9 +3,13 @@ package com.quizz.places.fragments;
 
 import java.util.ArrayList;
 
+import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,12 +17,14 @@ import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
 import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
 import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
@@ -50,6 +56,9 @@ public class GridLevelsFragment extends BaseListLevelsFragment {
         
         mTransitionLevel = (View) view.findViewById(R.id.transitionLevel);
         mTransitionLevelImage = (ImageView) mTransitionLevel.findViewById(R.id.levelPicture);
+        LinearLayout difficulty = (LinearLayout) mTransitionLevel.findViewById(R.id.levelDifficulty);
+        difficulty.setVisibility(View.GONE);
+        
         mLevelsGridView = (GridView) view.findViewById(R.id.gridLevels);
         mLevelsGridView.setAdapter(mAdapter);
         mLevelsGridView.setOnItemClickListener(mLevelItemClickListener);
@@ -128,39 +137,43 @@ public class GridLevelsFragment extends BaseListLevelsFragment {
 			RelativeLayout.LayoutParams params = (LayoutParams) mTransitionLevel.getLayoutParams();
 			params.width = view.getWidth();
 			params.height = view.getHeight();
-			params.setMargins(view.getTop(), view.getLeft(), 0, 0);
+			/*params.leftMargin = view.getLeft();
+			params.topMargin = view.getTop();*/
 			mTransitionLevel.setLayoutParams(params);
 			
 			/* Create scale & translate animations */
-			float scaleX = 2f;
-			float scaleY = 2f;
-			float pivotX = view.getLeft() + (view.getWidth() / 2);
-			float pivotY = view.getTop() + (view.getHeight() / 2);
-			float offsetX = view.getWidth() / scaleX;
-			float offsetY = view.getHeight() / scaleY;
+			float pivotX = view.getWidth() / 2;
+			float pivotY = view.getHeight() / 2;
+			float offsetX = view.getWidth() / 2;
+			float offsetY = view.getHeight() / 2;
+			
+			DisplayMetrics metrics = new DisplayMetrics();
+			getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+			// TODO: Height only on landscape mode
+			//metrics.heightPixels;
+			
+			int orientation = getResources().getConfiguration().orientation;
+			float scale = (orientation == Configuration.ORIENTATION_LANDSCAPE) ?
+					(float) metrics.heightPixels / (float) view.getHeight() : 
+					(float) metrics.widthPixels / (float) view.getWidth();
 			
 			AnimationSet animationSet = new AnimationSet(false);
-			animationSet.addAnimation(new ScaleAnimation(1f, 2f, 1f, 2f, pivotX, pivotY));
+			animationSet.addAnimation(new ScaleAnimation(1f, scale, 1f, scale, pivotX, pivotY));
 			animationSet.addAnimation(new TranslateAnimation(view.getLeft(), offsetX, 
 					view.getTop(), offsetY));
 			animationSet.setAnimationListener(mTransitionListener);
-			animationSet.setDuration(1000);
+			animationSet.setFillAfter(true);
+			animationSet.setDuration(300);
+			
+			RotateAnimation rotationAnimation = new RotateAnimation(0f, -12f);
+			rotationAnimation.setDuration(300);
+			rotationAnimation.setFillAfter(true);
+			mTransitionLevelImage.startAnimation(rotationAnimation);
 			
 			/* Fade out the grid */
 			Animation alphaAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.fade_out);
-			alphaAnimation.setAnimationListener(new AnimationListener() {
-				
-				@Override
-				public void onAnimationStart(Animation animation) {}
-				
-				@Override
-				public void onAnimationRepeat(Animation animation) {}
-				
-				@Override
-				public void onAnimationEnd(Animation animation) {
-					mLevelsGridView.setVisibility(View.GONE);
-				}
-			});
+			alphaAnimation.setFillAfter(true);
 			mLevelsGridView.startAnimation(alphaAnimation);
 			
 			/* Scale & translate the transition View */
