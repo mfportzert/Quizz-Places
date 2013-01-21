b150c38985237d34b8ca355b408b84743aac361f
mfportzert <michelf.portzert@gmail.com>
2013-01-21 01:47:43 +0100

    Formated source + changed access to QuizzDAO

--- src/com/quizz/places/fragments/StatsFragment.java
+++ src/com/quizz/places/fragments/StatsFragment.java
@@ -8,75 +8,74 @@ import android.widget.ListView;
 
 import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
 import com.quizz.core.activities.BaseQuizzActivity;
-import com.quizz.core.db.BaseQuizzDAO;
+import com.quizz.core.db.QuizzDAO;
 import com.quizz.core.fragments.BaseStatsFragment;
 import com.quizz.core.widgets.QuizzActionBar;
 import com.quizz.places.R;
 import com.quizz.places.adapters.StatsItemAdapter;
 
 public class StatsFragment extends BaseStatsFragment {
-	
-	private StatsItemAdapter mAdapter;
-	private ListView mStatsListView;
-	private boolean mHideActionBarOnDestroyView = true;
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mAdapter = new StatsItemAdapter(getActivity(), R.layout.item_stats);
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		super.onCreateView(inflater, container, savedInstanceState);
-		
-        View view = inflater.inflate(R.layout.fragment_stats, container, false);
 
-        BaseQuizzDAO dao = new BaseQuizzDAO();
-//        dao.getStats();
-        
-        mAdapter.addAll(dao.getStats());
-        mStatsListView = (ListView) view.findViewById(R.id.StatsListView);
-        mStatsListView.setAdapter(mAdapter);
-		
-		ObjectAnimator listDisplay = ObjectAnimator.ofFloat(mStatsListView, "alpha", 0f, 1f);
-        listDisplay.setDuration(300);
-        listDisplay.start();
-        
-        return view;
+    private StatsItemAdapter mAdapter;
+    private ListView mStatsListView;
+    private boolean mHideActionBarOnDestroyView = true;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	mAdapter = new StatsItemAdapter(getActivity(), R.layout.item_stats);
     }
-	
-	@Override
-	public void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	super.onCreateView(inflater, container, savedInstanceState);
+
+	View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+	mAdapter.addAll(QuizzDAO.INSTANCE.getStats());
+	mStatsListView = (ListView) view.findViewById(R.id.StatsListView);
+	mStatsListView.setAdapter(mAdapter);
+
+	ObjectAnimator listDisplay = ObjectAnimator.ofFloat(mStatsListView, "alpha", 0f, 1f);
+	listDisplay.setDuration(300);
+	listDisplay.start();
+
+	return view;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+	super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroyView() {
+	super.onDestroyView();
+	if (mHideActionBarOnDestroyView) {
+	    if (getActivity() instanceof BaseQuizzActivity) {
+		((BaseQuizzActivity) getActivity()).getQuizzActionBar().hide(
+			QuizzActionBar.MOVE_NORMAL);
+	    }
 	}
-	
-	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		if (mHideActionBarOnDestroyView) {
-			if (getActivity() instanceof BaseQuizzActivity) {
-				((BaseQuizzActivity) getActivity()).getQuizzActionBar().hide(QuizzActionBar.MOVE_NORMAL);
-			}
-		}
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+	super.onActivityCreated(savedInstanceState);
+	mHideActionBarOnDestroyView = true;
+	if (getActivity() instanceof BaseQuizzActivity) {
+	    ((BaseQuizzActivity) getActivity()).getQuizzActionBar().showIfNecessary(
+		    QuizzActionBar.MOVE_NORMAL);
 	}
-	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		mHideActionBarOnDestroyView = true;
-		if (getActivity() instanceof BaseQuizzActivity) {
-			((BaseQuizzActivity) getActivity()).getQuizzActionBar().showIfNecessary(QuizzActionBar.MOVE_NORMAL);
-		}
-	}
-	
-	@Override
+    }
+
+    @Override
     public void onPause() {
-        super.onPause();
+	super.onPause();
     }
-	
-	@Override
+
+    @Override
     public void onResume() {
-        super.onResume();
+	super.onResume();
     }
 }
