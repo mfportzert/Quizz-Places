4ecc5fe7089ab255117c5aed9fc3924a9db18d78
madyx <test.mouham@gmail.com>
2013-02-06 00:24:56 +0100

    New stats page.

--- src/com/quizz/places/adapters/StatsItemAdapter.java
+++ src/com/quizz/places/adapters/StatsItemAdapter.java
@@ -1,10 +1,13 @@
 package com.quizz.places.adapters;
 
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,18 +16,33 @@ import com.quizz.core.utils.ConvertUtils;
 import com.quizz.core.widgets.SectionProgressView;
 import com.quizz.places.R;
 
-public class StatsItemAdapter extends ArrayAdapter<Stat> {
+public class StatsItemAdapter extends BaseAdapter {
 
+	private List<Stat> mItems;
+	
+	private Context mContext;
+	
 	private int mLineLayout;
 	private LayoutInflater mInflater;
 	private int[] mProgressDrawables;
 	
-	public StatsItemAdapter(Context context, int lineLayout) {
-        super(context, lineLayout);
-        
-        mLineLayout = lineLayout;
-        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        mProgressDrawables = new int[] {
+	private static final int TYPE_SIMPLE_ITEM = 0;
+    private static final int TYPE_ACHIEVEMENT_ITEM = 1;
+    private static final int TYPE_COUNT = 2;
+    
+	static class ViewHolder {
+        ImageView icon;
+        TextView label;
+        TextView score;
+//        TextView average;
+        ImageView trophy;
+        SectionProgressView progress;
+    }
+
+	public StatsItemAdapter(Context context) {
+		mContext = context;
+		mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		mProgressDrawables = new int[] {
         		R.drawable.fg_section_progress_blue,
         		R.drawable.fg_section_progress_green,
         		R.drawable.fg_section_progress_orange,
@@ -33,48 +51,89 @@ public class StatsItemAdapter extends ArrayAdapter<Stat> {
         		R.drawable.fg_section_progress_yellow,
         		R.drawable.fg_section_progress_red
         };
+	}
+
+	public void setItems(List<Stat> items) {
+		this.mItems = items;
+	}
+
+	@Override
+    public int getItemViewType(int position) {
+        return getItem(position).isAchievement() ? TYPE_ACHIEVEMENT_ITEM : TYPE_SIMPLE_ITEM;
     }
 
-	static class ViewHolder {
-        TextView label;
-        TextView score;
-        TextView average;
-        ImageView icon;
-        SectionProgressView progress;
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_COUNT;
     }
+
+	@Override
+	public int getCount() {
+		return mItems.size();
+	}
+
+	@Override
+	public Stat getItem(int position) {
+		return mItems.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
 	
 	@SuppressWarnings("deprecation")
 	@Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
+		ViewHolder holder;
+		Stat stat = getItem(position);
+		int type = getItemViewType(position);
+		
+        System.out.println("getView " + position + " " + convertView + " type = " + type);
         if (convertView == null) {
-            
-        	convertView = this.mInflater.inflate(this.mLineLayout, null);
-        	
-            holder = new ViewHolder();
-            holder.label = (TextView) convertView.findViewById(R.id.StatLabel);
-            holder.score = (TextView) convertView.findViewById(R.id.StatDoneOnTotal);
-            holder.icon = (ImageView) convertView.findViewById(R.id.StatIcon);
-            holder.progress = (SectionProgressView) convertView.findViewById(R.id.StatProgress);
-                        
+        	holder = new ViewHolder();
+            switch (type) {
+            	case TYPE_SIMPLE_ITEM:
+                	convertView = mInflater.inflate(R.layout.item_simple_stat, null);
+                	holder.label = (TextView) convertView.findViewById(R.id.StatLabel);
+					holder.score = (TextView) convertView.findViewById(R.id.StatDoneOnTotal);
+					holder.icon = (ImageView) convertView.findViewById(R.id.StatIcon);
+                break;
+                case TYPE_ACHIEVEMENT_ITEM:
+                	convertView = mInflater.inflate(R.layout.item_achievement_stat, null);
+                    holder.label = (TextView) convertView.findViewById(R.id.StatLabel);
+					holder.score = (TextView) convertView.findViewById(R.id.StatDoneOnTotal);
+//					holder.average = (TextView) convertView.findViewById(R.id.StatPercentScore);
+					holder.icon = (ImageView) convertView.findViewById(R.id.StatIcon);
+					holder.progress = (SectionProgressView) convertView.findViewById(R.id.StatProgress);
+					holder.trophy = (ImageView) convertView.findViewById(R.id.StatCupIcon);
+		       	break;
+            }
             convertView.setTag(holder);
-            
         } else {
-            holder = (ViewHolder) convertView.getTag();
+    		holder = (ViewHolder) convertView.getTag();
         }
-        
-        Stat stat = getItem(position);
-       	holder.label.setText(stat.getLabel());
-       	holder.average.setText(String.valueOf(stat.getProgressInPercent()) + "%");
-       	holder.score.setText(String.valueOf(stat.getDone()) + " / " + String.valueOf(stat.getTotal()));
-       	holder.progress.setProgressRes(mProgressDrawables[position % mProgressDrawables.length]);
-       	holder.progress.setProgressValue(stat.getProgressInPercent());
-
-       	int verticalPadding = (int) ConvertUtils.convertDpToPixels(2.5f, getContext());
-       	int horizontalPadding = (int) ConvertUtils.convertDpToPixels(3f, getContext());
-       	holder.progress.setPaddingProgress(horizontalPadding, verticalPadding, 
-       			horizontalPadding, verticalPadding);
-       	
+		
+        if (stat.isAchievement() == true) {
+			holder.icon.setImageDrawable(this.mContext.getResources().getDrawable(stat.getIcon()));
+	       	holder.label.setText(String.valueOf(stat.getProgressInPercent()) + "% - " + stat.getLabel());
+//	       	holder.average.setText(String.valueOf(stat.getProgressInPercent()) + "%");
+	       	holder.score.setText(String.valueOf(stat.getDone()) + " / " + String.valueOf(stat.getTotal()));
+	       	holder.progress.setProgressRes(mProgressDrawables[position % mProgressDrawables.length]);
+	       	holder.progress.setProgressValue(stat.getProgressInPercent());
+	       	int verticalPadding = (int) ConvertUtils.convertDpToPixels(2.5f, this.mContext);
+	       	int horizontalPadding = (int) ConvertUtils.convertDpToPixels(3f, this.mContext);
+	       	holder.progress.setPaddingProgress(horizontalPadding, verticalPadding, 
+	       			horizontalPadding, verticalPadding);
+	       	if (stat.getDone() == stat.getTotal()) {
+	       		holder.trophy.setImageDrawable(this.mContext.getResources().getDrawable(R.drawable.gold_cup));
+	       	}
+        } else {
+    		holder.icon.setImageDrawable(this.mContext.getResources().getDrawable(stat.getIcon()));
+	       	holder.label.setText(stat.getLabel());
+	       	holder.score.setText(String.valueOf(stat.getDone()) + " / " + String.valueOf(stat.getTotal()));
+        }   	
         return convertView;
     }
+	
 }
--- src/com/quizz/places/fragments/StatsFragment.java
+++ src/com/quizz/places/fragments/StatsFragment.java
@@ -13,69 +13,77 @@ import com.quizz.core.fragments.BaseStatsFragment;
 import com.quizz.core.widgets.QuizzActionBar;
 import com.quizz.places.R;
 import com.quizz.places.adapters.StatsItemAdapter;
+import com.quizz.places.db.PlacesDAO;
 
 public class StatsFragment extends BaseStatsFragment {
+	private StatsItemAdapter mAdapter;
+	private ListView mStatsListView;
+	private boolean mHideActionBarOnDestroyView = true;
 
-    private StatsItemAdapter mAdapter;
-    private ListView mStatsListView;
-    private boolean mHideActionBarOnDestroyView = true;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	mAdapter = new StatsItemAdapter(getActivity(), R.layout.item_stats);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-	super.onCreateView(inflater, container, savedInstanceState);
-
-	View view = inflater.inflate(R.layout.fragment_stats, container, false);
-
-//	mAdapter.addAll(QuizzDAO.INSTANCE.getStats());
-//	mStatsListView = (ListView) view.findViewById(R.id.StatsListView);
-//	mStatsListView.setAdapter(mAdapter);
-
-	ObjectAnimator listDisplay = ObjectAnimator.ofFloat(mStatsListView, "alpha", 0f, 1f);
-	listDisplay.setDuration(300);
-	listDisplay.start();
-
-	return view;
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-	super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onDestroyView() {
-	super.onDestroyView();
-	if (mHideActionBarOnDestroyView) {
-	    if (getActivity() instanceof BaseQuizzActivity) {
-		((BaseQuizzActivity) getActivity()).getQuizzActionBar().hide(
-			QuizzActionBar.MOVE_NORMAL);
-	    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mAdapter = new StatsItemAdapter(getActivity());
+		((BaseQuizzActivity) getActivity()).getQuizzActionBar().getMiddleText()
+				.setText("Statistics");
+		((BaseQuizzActivity) getActivity()).getQuizzActionBar().getRightText()
+				.setText("");
 	}
-    }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-	super.onActivityCreated(savedInstanceState);
-	mHideActionBarOnDestroyView = true;
-	if (getActivity() instanceof BaseQuizzActivity) {
-	    ((BaseQuizzActivity) getActivity()).getQuizzActionBar().showIfNecessary(
-		    QuizzActionBar.MOVE_NORMAL);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+
+		View view = inflater.inflate(R.layout.fragment_stats, container, false);
+
+		PlacesDAO dao = new PlacesDAO(this.getActivity());
+
+		mAdapter.setItems(dao.getStats());
+		mStatsListView = (ListView) view.findViewById(R.id.StatsListView);
+		mStatsListView.setAdapter(mAdapter);
+
+		ObjectAnimator listDisplay = ObjectAnimator.ofFloat(mStatsListView,
+				"alpha", 0f, 1f);
+		listDisplay.setDuration(300);
+		listDisplay.start();
+
+		return view;
 	}
-    }
 
-    @Override
-    public void onPause() {
-	super.onPause();
-    }
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
 
-    @Override
-    public void onResume() {
-	super.onResume();
-    }
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		if (mHideActionBarOnDestroyView) {
+			if (getActivity() instanceof BaseQuizzActivity) {
+				((BaseQuizzActivity) getActivity()).getQuizzActionBar().hide(
+						QuizzActionBar.MOVE_NORMAL);
+			}
+		}
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mHideActionBarOnDestroyView = true;
+		if (getActivity() instanceof BaseQuizzActivity) {
+			((BaseQuizzActivity) getActivity()).getQuizzActionBar()
+					.showIfNecessary(QuizzActionBar.MOVE_NORMAL);
+		}
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
 }
