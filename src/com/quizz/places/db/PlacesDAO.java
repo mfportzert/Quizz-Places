ef90c100e02cf9fb5cada1f7b94db782907877d3
madyx <test.mouham@gmail.com>
2013-03-08 22:22:05 +0100

    settings view improvements. Multi languages support for stats screen.

--- src/com/quizz/places/db/PlacesDAO.java
+++ src/com/quizz/places/db/PlacesDAO.java
@@ -3,19 +3,18 @@ package com.quizz.places.db;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.R;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.util.Log;
 
-import com.quizz.core.application.BaseQuizzApplication;
 import com.quizz.core.db.DbHelper;
 import com.quizz.core.db.QuizzDAO;
 import com.quizz.core.models.Hint;
 import com.quizz.core.models.Level;
 import com.quizz.core.models.Section;
 import com.quizz.core.models.Stat;
+import com.quizz.places.R;
 
 public class PlacesDAO {
 
@@ -96,27 +95,27 @@ public class PlacesDAO {
 
 		cursor.moveToFirst();
 		stats.add(new Stat(mContext.getResources().getIdentifier("sections",
-				"drawable", "com.quizz.places"), "Niveaux débloqués", cursor
+				"drawable", "com.quizz.places"), mContext.getString(R.string.unlocked_lvl), cursor
 				.getInt(cursor.getColumnIndex("sections_unlocked")), cursor
 				.getInt(cursor.getColumnIndex("sections_total")), true));
 		stats.add(new Stat(mContext.getResources().getIdentifier("levels",
-				"drawable", "com.quizz.places"), "Images trouvées", cursor
+				"drawable", "com.quizz.places"), mContext.getString(R.string.places_found), cursor
 				.getInt(cursor.getColumnIndex("levels_clear")), cursor
 				.getInt(cursor.getColumnIndex("levels_total")), true));
 		stats.add(new Stat(mContext.getResources().getIdentifier("easy",
-				"drawable", "com.quizz.places"), "Trouvées en facile", cursor
+				"drawable", "com.quizz.places"), mContext.getString(R.string.easy_found), cursor
 				.getInt(cursor.getColumnIndex("levels_easy_clear")), cursor
 				.getInt(cursor.getColumnIndex("levels_easy_total")), true));
 		stats.add(new Stat(mContext.getResources().getIdentifier("medium",
-				"drawable", "com.quizz.places"), "Trouvées en moyen", cursor
+				"drawable", "com.quizz.places"), mContext.getString(R.string.medium_found), cursor
 				.getInt(cursor.getColumnIndex("levels_medium_clear")), cursor
 				.getInt(cursor.getColumnIndex("levels_medium_total")), true));
 		stats.add(new Stat(mContext.getResources().getIdentifier("hard",
-				"drawable", "com.quizz.places"), "Trouvées en difficile",
+				"drawable", "com.quizz.places"), mContext.getString(R.string.hard_found),
 				cursor.getInt(cursor.getColumnIndex("levels_hard_clear")),
 				cursor.getInt(cursor.getColumnIndex("levels_hard_total")), true));
 		stats.add(new Stat(mContext.getResources().getIdentifier("hint",
-				"drawable", "com.quizz.places"), "Indices utilisés", cursor
+				"drawable", "com.quizz.places"), mContext.getString(R.string.used_hints), cursor
 				.getInt(cursor.getColumnIndex("revealed_hints")), cursor
 				.getInt(cursor.getColumnIndex("total_hints")), false));
 		cursor.close();
