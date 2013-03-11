0280010378da5ca864247d426c6df1c784bc984f
mfportzert <michelf.portzert@gmail.com>
2013-03-11 02:08:45 +0100

    Updated item_list_sections.xml to support locked sections. Added a lock resource.

--- src/com/quizz/places/adapters/SectionsItemAdapter.java
+++ src/com/quizz/places/adapters/SectionsItemAdapter.java
@@ -1,6 +1,7 @@
 package com.quizz.places.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,23 +16,33 @@ import com.quizz.places.R;
 
 public class SectionsItemAdapter extends ArrayAdapter<Section> {
 
+	private static final int[] PROGRESS_DRAWABLES = new int[] { 
+		R.drawable.fg_section_progress_blue,
+		R.drawable.fg_section_progress_green,
+		R.drawable.fg_section_progress_orange,
+		R.drawable.fg_section_progress_pink,
+		R.drawable.fg_section_progress_purple,
+		R.drawable.fg_section_progress_yellow,
+		R.drawable.fg_section_progress_red
+	};
+	
 	private int mLineLayout;
 	private LayoutInflater mInflater;
-	private int[] mProgressDrawables;
-
+	private Drawable[] mProgressDrawables = new Drawable[PROGRESS_DRAWABLES.length];
+	private Drawable mLockDrawable;
+	
 	public SectionsItemAdapter(Context context, int lineLayout) {
 		super(context, lineLayout);
 
 		mLineLayout = lineLayout;
 		mInflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mProgressDrawables = new int[] { R.drawable.fg_section_progress_blue,
-				R.drawable.fg_section_progress_green,
-				R.drawable.fg_section_progress_orange,
-				R.drawable.fg_section_progress_pink,
-				R.drawable.fg_section_progress_purple,
-				R.drawable.fg_section_progress_yellow,
-				R.drawable.fg_section_progress_red };
+		
+		// Preload Drawables to gain performance inside getView()
+		for (int i = 0; i < PROGRESS_DRAWABLES.length; i++) {
+			mProgressDrawables[i] = context.getResources().getDrawable(PROGRESS_DRAWABLES[i]);
+		}
+		mLockDrawable = context.getResources().getDrawable(R.drawable.ic_lock);
 	}
 
 	static class ViewHolder {
@@ -39,6 +50,9 @@ public class SectionsItemAdapter extends ArrayAdapter<Section> {
 		TextView levels;
 		ImageView buttonEnter;
 		SectionProgressView progress;
+		
+		// In order to bring the background in top of the view when section is locked
+		View background;
 	}
 
 	@SuppressWarnings("deprecation")
@@ -51,13 +65,11 @@ public class SectionsItemAdapter extends ArrayAdapter<Section> {
 
 			holder = new ViewHolder();
 			holder.name = (TextView) convertView.findViewById(R.id.sectionName);
-			holder.levels = (TextView) convertView
-					.findViewById(R.id.sectionNbLevels);
-			holder.buttonEnter = (ImageView) convertView
-					.findViewById(R.id.sectionEnterButton);
-			holder.progress = (SectionProgressView) convertView
-					.findViewById(R.id.sectionProgress);
-
+			holder.levels = (TextView) convertView.findViewById(R.id.sectionNbLevels);
+			holder.buttonEnter = (ImageView) convertView.findViewById(R.id.sectionEnterButton);
+			holder.progress = (SectionProgressView) convertView.findViewById(R.id.sectionProgress);
+			holder.background = convertView.findViewById(R.id.section_content_bg);
+			
 			holder.buttonEnter.setAlpha(225);
 
 			convertView.setTag(holder);
@@ -68,17 +80,24 @@ public class SectionsItemAdapter extends ArrayAdapter<Section> {
 
 		Section section = getItem(position);
 		holder.name.setText(section.name);
-		holder.progress.setProgressRes(mProgressDrawables[position
+		holder.progress.setProgressDrawable(mProgressDrawables[position
 				% mProgressDrawables.length]);
 		holder.progress.setProgressValue(34);
 
-		int verticalPadding = (int) ConvertUtils.convertDpToPixels(2.5f,
-				getContext());
-		int horizontalPadding = (int) ConvertUtils.convertDpToPixels(3f,
-				getContext());
+		int verticalPadding = (int) ConvertUtils.convertDpToPixels(2.5f, getContext());
+		int horizontalPadding = (int) ConvertUtils.convertDpToPixels(3f, getContext());
 		holder.progress.setPaddingProgress(horizontalPadding, verticalPadding,
 				horizontalPadding, verticalPadding);
 
+		// Level locked management
+		if (position > 0) {
+			holder.name.setCompoundDrawablesWithIntrinsicBounds(null, null, mLockDrawable, null);
+			holder.background.bringToFront();
+			holder.name.bringToFront();
+		} else {
+			holder.name.setCompoundDrawables(null, null, null, null);
+		}
+		
 		return convertView;
 	}
 }
