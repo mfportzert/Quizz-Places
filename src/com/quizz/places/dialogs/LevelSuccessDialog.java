873e9e8ae41f77be0b6c5f1cec60064a18e0bd5a
mfportzert <michelf.portzert@gmail.com>
2013-03-17 21:05:22 +0100

    Added a rotating rays effect on LevelSuccessDialog

--- src/com/quizz/places/dialogs/LevelSuccessDialog.java
+++ src/com/quizz/places/dialogs/LevelSuccessDialog.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Window;
+import android.view.animation.LinearInterpolator;
 
+import com.actionbarsherlock.internal.nineoldandroids.animation.ValueAnimator;
+import com.actionbarsherlock.internal.nineoldandroids.animation.ValueAnimator.AnimatorUpdateListener;
 import com.quizz.places.R;
+import com.quizz.places.widgets.RotatingSunEffect;
 
 public class LevelSuccessDialog extends Activity {
 
+	private ValueAnimator mBackgroundRotationAnimator;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -18,5 +24,29 @@ public class LevelSuccessDialog extends Activity {
 		getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
 		setContentView(R.layout.dialog_level_success);
+		
+		final RotatingSunEffect background = (RotatingSunEffect) findViewById(R.id.level_success_background_effect);
+		
+		mBackgroundRotationAnimator = ValueAnimator.ofFloat(0, 360);
+		mBackgroundRotationAnimator.addUpdateListener(new AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator arg0) {
+
+                background.rotationDegrees = (Float) arg0.getAnimatedValue();
+                background.invalidate();
+            }
+        });
+		mBackgroundRotationAnimator.setInterpolator(new LinearInterpolator());
+		mBackgroundRotationAnimator.setRepeatCount(ValueAnimator.INFINITE);
+		mBackgroundRotationAnimator.setDuration(6500);
+		mBackgroundRotationAnimator.start();
+	}
+	
+	@Override
+	protected void onDestroy() {
+		if (mBackgroundRotationAnimator != null && mBackgroundRotationAnimator.isRunning()) {
+			mBackgroundRotationAnimator.end();
+		}
+		super.onDestroy();
 	}
 }
---
+++ src/com/quizz/places/widgets/RotatingSunEffect.java
@@ -0,0 +1,151 @@
+package com.quizz.places.widgets;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.RadialGradient;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class RotatingSunEffect extends View {
+
+	private static final int RAYS_COLOR = 0x22FFB90F;
+	
+	public float rotationDegrees = 0f;
+	private Point mCenterPoint = new Point();
+	private Point mPointB = new Point();
+	private Point mPointC = new Point();
+	private Paint mRayPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	private Paint mInnerGlowPaint = new Paint();
+
+	private Bitmap mSavedRaysBitmap = null;
+	private Canvas mSavedRaysCanvas = null;
+
+	private Bitmap mSavedInnerGlowBitmap = null;
+	private Canvas mSavedInnerGlowCanvas = null;
+
+	private void init() {
+		mRayPaint.setStrokeWidth(2);
+		mRayPaint.setColor(RAYS_COLOR);
+		mRayPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+		mRayPaint.setAntiAlias(true);
+
+		mInnerGlowPaint = new Paint();
+		mInnerGlowPaint.setColor(0xFF000000);
+		mInnerGlowPaint.setXfermode(new PorterDuffXfermode(Mode.DST_OUT));
+	}
+
+	public RotatingSunEffect(Context context) {
+		super(context);
+		init();
+	}
+
+	public RotatingSunEffect(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public RotatingSunEffect(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+
+	private void drawFoggyWindowWithTransparentCircle(Canvas canvas,
+			Bitmap bitmap, float circleX, float circleY, float radius) {
+
+		if (mSavedInnerGlowBitmap == null) {
+			// Create a temporary bitmap
+			mSavedInnerGlowBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+					bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+			mSavedInnerGlowCanvas = new Canvas(mSavedInnerGlowBitmap);
+		}
+		
+		// Clear the bitmap of previous drawing
+		mSavedInnerGlowBitmap.eraseColor(Color.TRANSPARENT);
+		
+		// Copy bitmap into tempBitmap
+		mSavedInnerGlowCanvas.drawBitmap(bitmap, 0, 0, null);
+
+		// Create a radial gradient
+		RadialGradient gradient = new android.graphics.RadialGradient(circleX,
+				circleY, radius, 0xFF000000, 0x00000000,
+				android.graphics.Shader.TileMode.CLAMP);
+
+		mInnerGlowPaint.setShader(gradient);
+
+		// Draw transparent circle into tempBitmap
+		mSavedInnerGlowCanvas.drawCircle(circleX, circleY, radius,
+				mInnerGlowPaint);
+
+		// Draw tempBitmap onto the screen (over what's already there)
+		canvas.drawBitmap(mSavedInnerGlowBitmap, 0, 0, null);
+	}
+
+	@Override
+	public void draw(Canvas canvas) {
+		super.draw(canvas);
+
+		// angle lumineux : 20
+		// angle ecart : 40
+
+		mCenterPoint.x = (int) (getWidth() * 0.4f);
+		mCenterPoint.y = (int) (getHeight());
+
+		// Initialize only once
+		if (mSavedRaysBitmap == null) {
+			mSavedRaysBitmap = Bitmap.createBitmap((int) getWidth(),
+					(int) getHeight(), Config.ARGB_4444);
+			mSavedRaysCanvas = new Canvas(mSavedRaysBitmap);
+		} else {
+
+			// Clear the bitmap of previous drawing
+			mSavedRaysBitmap.eraseColor(Color.TRANSPARENT);
+
+			int distance = (int) Math.sqrt(Math.pow(getWidth(), 2)
+					+ Math.pow(getHeight(), 2));
+			int rotationStart = (int) rotationDegrees;
+			for (int angle = rotationStart; angle < rotationStart + 360; angle += 60) {
+
+				double thetaB = Math.toRadians(angle);
+				mPointB.x = (int) (mCenterPoint.x + distance * Math.cos(thetaB));
+				mPointB.y = (int) (mCenterPoint.y + distance * Math.sin(thetaB));
+
+				double thetaC = Math.toRadians(angle + 20);
+				mPointC.x = (int) (mCenterPoint.x + distance * Math.cos(thetaC));
+				mPointC.y = (int) (mCenterPoint.y + distance * Math.sin(thetaC));
+
+				Path path = new Path();
+				path.setFillType(Path.FillType.EVEN_ODD);
+				path.moveTo(mCenterPoint.x, mCenterPoint.y);
+				path.lineTo(mPointB.x, mPointB.y);
+				path.lineTo(mPointC.x, mPointC.y);
+				path.close();
+
+				thetaB = Math.toRadians(angle + 20);
+				mPointB.x = (int) (mCenterPoint.x + distance * Math.cos(thetaB));
+				mPointB.y = (int) (mCenterPoint.y + distance * Math.sin(thetaB));
+
+				thetaC = Math.toRadians(angle + 60);
+				mPointC.x = (int) (mCenterPoint.x + distance * Math.cos(thetaC));
+				mPointC.y = (int) (mCenterPoint.y + distance * Math.sin(thetaC));
+
+				mSavedRaysCanvas.drawPath(path, mRayPaint);
+			}
+		}
+
+		drawFoggyWindowWithTransparentCircle(canvas, mSavedRaysBitmap,
+				mCenterPoint.x, mCenterPoint.y, getWidth() / 1.6f);
+	}
+}
