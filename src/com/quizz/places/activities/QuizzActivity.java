d6ca414ef836a257961ea71c5862126ed7456be4
mfportzert <michelf.portzert@gmail.com>
2013-03-19 01:42:43 +0100

    Cleaned code of QuizzActivity

--- src/com/quizz/places/activities/QuizzActivity.java
+++ src/com/quizz/places/activities/QuizzActivity.java
@@ -1,10 +1,12 @@
 package com.quizz.places.activities;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
 import android.view.animation.LinearInterpolator;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.actionbarsherlock.internal.nineoldandroids.animation.ObjectAnimator;
 import com.quizz.core.activities.BaseQuizzActivity;
@@ -14,11 +16,10 @@ import com.quizz.places.R;
 import com.quizz.places.fragments.MenuFragment;
 
 public class QuizzActivity extends BaseQuizzActivity {
-
 	private static final String JSON_FILE = "places.json";
-
+	
 	private ObjectAnimator mBgAnimation;
-
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -32,25 +33,11 @@ public class QuizzActivity extends BaseQuizzActivity {
 					getSupportFragmentManager(), this, false, transaction);
 		}
 
-		/*
-		 * View confirmQuitDialog =
-		 * getLayoutInflater().inflate(R.layout.dialog_confirm_quit, null);
-		 * setConfirmQuitDialogView(confirmQuitDialog);
-		 */
-
 		getQuizzActionBar().getBackButton().setImageResource(
 				R.drawable.back_but_5);
 		getQuizzActionBar().setCustomView(R.layout.ab_view_sections);
 		getQuizzActionBar().setBackgroundResource(R.drawable.bg_actionbar);
-
-		View customView = getQuizzActionBar().getCustomViewContainer();
-		TextView middleText = (TextView) customView
-				.findViewById(R.id.ab_section_middle_text);
-		TextView rightText = (TextView) customView
-				.findViewById(R.id.ab_section_right_text);
-		middleText.setText("22 / 30");
-		rightText.setText("345 pts");
-
+		
 		initBackground();
 	}
 
