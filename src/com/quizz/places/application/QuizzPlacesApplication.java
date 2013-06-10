f1ff1fd0c89e568f8da06034a2be1862b0bae490
madyx <test.mouham@gmail.com>
2013-06-10 23:14:51 +0200

    acra.

--- src/com/quizz/places/application/QuizzPlacesApplication.java
+++ src/com/quizz/places/application/QuizzPlacesApplication.java
@@ -1,9 +1,28 @@
 package com.quizz.places.application;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.annotation.ReportsCrashes;
+
 import com.quizz.core.application.BaseQuizzApplication;
 
+@ReportsCrashes(formKey = "",
+				mailTo = "tagsphere.droid@gmail.com",
+				customReportContent = 
+					{
+						ReportField.APP_VERSION_CODE,
+						ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL,
+						ReportField.CUSTOM_DATA, ReportField.STACK_TRACE
+					},
+				formUri = "https://tagspheredroid.cloudant.com/acra-worldtourquiz/_design/acra-storage/_update/report")
 public class QuizzPlacesApplication extends BaseQuizzApplication {
 
 	public static final String IMAGES_DIR = "images/";
 	public static final String JSON_FILE = "places.json";
+	
+	public void onCreate()
+	{
+		super.onCreate();
+		ACRA.init(this);
+	}
 }
